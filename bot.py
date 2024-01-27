ff8e58edff00decefa3bd1474064ea6105787cb9
allamurodxakimov <xakimov2806@gmail.com>
2024-01-27 15:37:33 +0300

    echo bot flask

---
+++ bot.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import telegram
+import os
+
+bot = os.getenv('TOKEN')
+app = Flask(__name__)
+
+user = telegram.Bot(bot)
+chat_id = "6497116565"
+@app.route('/')
+def home_page():
+    user.send_message(chat_id = chat_id, text = "Assalomu aleykum")
+    return "Hello programmer"
