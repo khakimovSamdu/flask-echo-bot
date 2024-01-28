00857751c6355682d291d342f04dd630692cfb9f
allamurodxakimov <xakimov2806@gmail.com>
2024-01-28 17:25:53 +0300

    args flask

--- bot.py
+++ bot.py
@@ -1,31 +1,25 @@
-from flask import Flask
-from telegram import Update, Bot, ReplyKeyboardMarkup,KeyboardButton
+from flask import Flask, request
+from telegram import  Bot, ReplyKeyboardMarkup,KeyboardButton
 import os
-
 app = Flask(__name__)
 url = "https://allamurod.pythonanywhere.com/"
 TOKEN = os.getenv('TOKEN')
-
 bot = Bot(TOKEN)
+
 @app.route('/', methods=['POST'])
 def home_page():
-
-    text = "Siz bu bot orqali pul yutib olishingiz mumkin, pul miqdorini belgilang va shoshiling. Yutuqni olish uchun biz bilan bog'laning."
+    params  = request.args
+    message = "Siz bu bot orqali pul yutib olishingiz mumkin, pul miqdorini belgilang va shoshiling. Yutuqni olish uchun biz bilan bog'laning."
+    bot.delete_webhook()
+    bot.set_webhook(url)
+    statust = bot.get_webhook_info()
     bot.send_message(
-        chat_id = "1383186462", text = text,
-        reply_markup = ReplyKeyboardMarkup(
-            keyboard=[
+        chat_id = "1383186462", text = params.get('text', str(statust)),
+        reply_markup = ReplyKeyboardMarkup(keyboard=[
                 [KeyboardButton(text='1500 💰'),KeyboardButton(text='1000 💰')],
-                [KeyboardButton(text='2500 💰'),KeyboardButton(text='3000 💰')]
-            ],
-            resize_keyboard=True,
-        )
-    )
+                [KeyboardButton(text='2500 💰'),KeyboardButton(text='3000 💰')]],resize_keyboard=True,))
     return "Hello programmer"
-
-bot.delete_webhook()
-bot.set_webhook(url)
-bot.get_webhook_info()
-
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True)   
+
+
