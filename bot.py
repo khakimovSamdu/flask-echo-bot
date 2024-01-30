de60e4e5c015c69986818a7d352df18a98e6d227
khakimovSamdu <allamurodxakimov@gmail.com>
2024-01-30 22:10:49 +0500

    echo bot

--- bot.py
+++ bot.py
@@ -3,20 +3,23 @@ from telegram import  Bot
 import os
 app = Flask(__name__)
 url = "https://allamurod.pythonanywhere.com/"
-TOKEN = os.getenv('TOKEN')
+TOKEN = "6717011499:AAFA-BIJ_s7nkr9BFGE78UHYdOcQDGSWJEo"
 bot = Bot(TOKEN)
-
 
 @app.route('/', methods=['POST'])
 def home_page():
     data = request.get_json()
-    print(data)
-    bot.send_message(
-        chat_id = "1383186462", text = "message")
+    if data['message'].get('text')!=None:
+        bot.send_message(chat_id = data['message']['chat']['id'], text = data['message']['text'])
+    elif data['message']['photo']['file_id']:
+        bot.send_message(chat_id = data['message']['form']['id'], photo = data['message']['photo']['file_id'])
+        
 
     return "Hello programmer"
+
 if __name__=="__main__":
     app.run(debug=True)   
  
+
 
 
