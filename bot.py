70567c886174d86c4662634bc8a5d153644bd88e
khakimovSamdu <allamurodxakimov@gmail.com>
2024-01-31 11:01:30 +0500

    echo

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from telegram import  Bot
-import os
+
 app = Flask(__name__)
 url = "https://allamurod.pythonanywhere.com/"
 TOKEN = "6717011499:AAFA-BIJ_s7nkr9BFGE78UHYdOcQDGSWJEo"
@@ -9,14 +9,15 @@ bot = Bot(TOKEN)
 @app.route('/', methods=['POST'])
 def home_page():
     data = request.get_json()
+    print(data)
+    user = data['message']['chat']
     if data['message'].get('text')!=None:
-        bot.send_message(chat_id = data['message']['chat']['id'], text = data['message']['text'])
-    elif data['message']['photo']['file_id']:
-        bot.send_message(chat_id = data['message']['form']['id'], photo = data['message']['photo']['file_id'])
+        bot.send_message(chat_id = user['id'], text = data['message']['text'])
+    elif data['message'].get('photo')!=None:
+        bot.send_photo(chat_id = user['id'], photo = data['message']['photo'][0]['file_id'])
         
-
     return "Hello programmer"
-
+bot.delete_webhook()
 if __name__=="__main__":
     app.run(debug=True)   
  
